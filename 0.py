d0283a99a8d970e29057fcd8a24e0c61af0a9584
Matheus Barros <matheuspereirabarros@gmail.com>
2017-01-25 16:27:13 -0300

    Exercicio 01 Programacao WEB

---
+++ 0.py
@@ -0,0 +1,16 @@
+pao = 0.12
+broa = 1.50
+valor = 0
+
+def fatura(quantPao, quantBroa):
+    valor = (pao*quantPao)+(broa*quantBroa)
+    print("A fatura do dia é: ", valor, "R$")
+    print("Você deve guardar", valor*0.10, "R$ na poupança")
+    
+
+def main():
+    #"1 2".split() = ["1", "2"]
+    quantPao, quantBroa = [int(i) for i in input("Digite a quantidade de pão e broa vendidos hoje: ").split()]
+    fatura(quantPao, quantBroa)
+
+main()
